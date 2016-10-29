0be103479b240ff0602c095e8e569200ab6bc696
Alex Huang <alexh95@mit.edu>
2016-10-29 20:51:17 +0800

    Added firmware level control for 2048 game flow
    -Added helper functions for spawning new blocks (determining to spawn 2
    or 4)
    -Print board function implemented, uses escape sequence to move cursor
    up so user doesn't see a whole bunch of new boards being printed

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ void sendBytePrintf(uint8_t byte, FILE *stream);
 uint8_t recieveByte(void);
 uint16_t adcRead(void);
 uint16_t getSeed(void);
+Boolean spawnTwo(void);
+void printBoard(Board *board);
+void play2048(void);
 
 //Set up stream to use to redirect stdout characters  to UART send function 
 static FILE uartSTDOUT = FDEV_SETUP_STREAM(sendBytePrintf,NULL,_FDEV_SETUP_WRITE);
@@ -33,7 +36,8 @@ void setup(void) {
     //Bind stdout to print via UART
     stdout = &uartSTDOUT;
     setupADC();
-    srand(getSeed());
+    srandom(getSeed());
+    _delay_ms(3000);
 }
 
 /**
@@ -108,18 +112,74 @@ uint16_t getSeed(void) {
 }
 
 /**
+ * Helper function to determine whether to spawn 2 or 4.
+ * Returns True if a 2 should be spawned.
+ */
+Boolean spawnTwo(void) {
+    //Spawn two 90% of the time
+    uint8_t prob = random() % 10;
+    return !(prob == 9);
+} 
+
+/**
+ * Helper method to print the board to UART
+ */
+void printBoard(Board *board) {
+    uint16_t boardVal;
+
+    //Get cursor back to original position
+    printf("%c[9A",27);
+
+    printf("#-------------------#\n");
+    for (uint8_t row = 0; row < 4; row++) {
+        printf("|");
+        for (uint8_t col = 0; col < 4; col++) {
+            boardVal = board -> grid[row][col].value;
+            if (boardVal)
+                printf("%4u|",board -> grid[row][col].value); 
+            else
+                printf("    |");
+        }
+        printf("\n");
+        printf("#-------------------#\n");
+    }
+}
+
+/**
  * 2048 Game 
  */
-void play2048(void) {
-     
+void play2048(void) { 
+     Board board = Board_newBlankBoard();  
+     //Put down two random tiles first
+     Board_putRandom(&board, random(),spawnTwo());
+     Board_putRandom(&board, random(),spawnTwo());
+     uint8_t recievedByte;
+     Direction dir;
+     printBoard(&board);
+     while (!Board_gameWon(&board)) {
+         recievedByte = recieveByte();
+         if (recievedByte == 'j') 
+             dir = LEFT;
+         else if (recievedByte == 'l')
+             dir = RIGHT;
+         else if (recievedByte == 'i')
+             dir = UP;
+         else if (recievedByte == 'k')
+             dir = DOWN;
+         else
+             continue;
+         if (Board_shift(dir,&board)) {
+            Board_putRandom(&board, random(), spawnTwo());
+            printBoard(&board);
+         }
+     }
 }
 
 int main(void)
 {
     setup();
-    uint8_t recieved_byte;
     for(;;){
-
+        play2048();
     }
     return 0;   /* never reached */
 }
